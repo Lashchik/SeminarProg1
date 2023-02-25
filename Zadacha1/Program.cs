d1141ddea84609c7385e25c36721ba6ed38574b6
Lashchik <Pondohva69@mail.ru>
2023-02-25 15:13:13 +0300

    сделал ДЗ

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -8,13 +8,13 @@
 Console.WriteLine("Введите целое число");
 int num = Convert.ToInt32(Console.ReadLine());
 int res = num * num;
-Console.WriteLine("Квадрат введённого числа равен"+res);
+Console.WriteLine("Квадрат введённого числа равен "+res);
 
 if (num == 5)
 {
-Console.WriteLine("пятёрочка");
+Console.WriteLine("это пятёрочка");
 }
 else if (num==3)
-Console.WriteLine("троечка");
+Console.WriteLine("это троечка");
 else
-Console.WriteLine("не пятёрочка и не троечка");
+Console.WriteLine("это не пятёрочка и не троечка");
--- ZadachaHARD/Program.cs
+++ ZadachaHARD/Program.cs
@@ -4,3 +4,12 @@
 // 9146548 -> 4
 // 3 -> нет
 
+Console.WriteLine("Введи число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string numText = Convert.ToString(num);
+if (numText.Length > 2){
+  Console.WriteLine("третья цифра -> " + numText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
