8905d5602d8684d9e5c1a3b21c95c2af043ded1a
Lashchik <Pondohva69@mail.ru>
2023-03-05 23:16:32 +0300

    save

--- Seminar3HomeWork/DZ№21/Program.cs
+++ Seminar3HomeWork/DZ№21/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координаты первой точки X1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты первой точки Y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты первой точки Z1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки X2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки Y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты первой точки Z2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double result = Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2));
+Console.WriteLine(result);
---
+++ Seminar3HomeWork/DZ№21Hard/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 21 - HARD необязательная
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в N-мерном пространстве. Сначала задается N с клавиатуры, потом задаются координаты точек.
+
+Console.WriteLine("введите размерность пространства");
+int num = Convert.ToInt32(Console.ReadLine());
+double [] kordX = new double [num];
+double [] kordY = new double [num];
+int index = 0;
+while (index < num)
+{Console.WriteLine("введите координату точки X" + (index+1));
+kordX [index] = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите координату точки Y" + (index+1));
+kordY [index] = Convert.ToDouble(Console.ReadLine());
+double result = Math.Sqrt((kordX[index]) - (kordY[index])*(kordX[index]) - (kordY[index]));
+Console.WriteLine(result);
+index++;}
