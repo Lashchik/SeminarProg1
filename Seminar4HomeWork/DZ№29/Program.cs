4625527f9bd40d5ac4e5ea637688c6af0b7f3bfb
Lashchik <Pondohva69@mail.ru>
2023-03-10 19:41:57 +0300

    DZ Seminar4

---
+++ Seminar4HomeWork/DZ№29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array = new int[8];
+Console.WriteLine("Введите 8 целых чисел:");
+for (int i = 0; i < array.Length; i++) 
+{
+Console.Write($"Число {i + 1}: ");
+array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Массив:");
+Console.WriteLine(string.Join(" ", array));
