c82396bc29f13defd9968cda2bf7ddea9cb3c96f
Lashchik <Pondohva69@mail.ru>
2023-03-11 13:28:36 +0300

    Семинар 4

---
+++ Seminar5/Zadacha32/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+void Array(int [] array)
+{
+    for (int i =0; i<array.Length; i++)
+        array[i] = new Random().Next(-9,10);
+}
+
+void Reverse(int [] array)
+{
+    for (int i =0; i<array.Length; i++)
+        array[i] = -array[i];
+}
+
+void PrintArray(int [] array)
+{
+    foreach (int el in array)
+    {
+        Console.Write($"{el} ");
+    }
+    Console.WriteLine();
+}
+
+int [] array  = new int [4];
+Array(array);
+PrintArray(array);
+Reverse(array);
+PrintArray(array);
---
+++ Seminar5/Zadacha33/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+void Array(int [] array)
+{
+    for (int i =0; i<array.Length; i++)
+        array[i] = new Random().Next(-9,10);
+}
+
+void PrintArray(int [] array)
+{
+    foreach (int el in array)
+    {
+        Console.Write($"{el} ");
+    }
+    Console.WriteLine();
+}
+
+void Poisk(int[] array)
+{
+    Console.Write("Введите ваше число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    bool find = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == num || array[i] == num*(-1))
+        {
+            // Console.WriteLine("Ваше число есть в массиве");
+            find = true;
+            break;
+        }
+    }
+    Console.WriteLine($"Результат поиска {find}");
+}
+
+
+int [] array  = new int [10];
+Array(array);
+PrintArray(array);
+Poisk(array);
