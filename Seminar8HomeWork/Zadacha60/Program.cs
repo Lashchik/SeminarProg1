b38ca53fa3a9f0814e847711a63dc48d9345d233
Aleksandr V. Loshсhilov <Aleksandr.Loshchilov@intourist.ru>
2023-03-24 17:54:52 +0300

    DZ62

---
+++ Seminar8HomeWork/Zadacha60/Program.cs
@@ -0,0 +1,102 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+// int[,,] array = new int[10, 10, 10];
+// FillArray(array);
+// PrintArray(array);
+// bool contains = ArrayContains(array, 42);
+// Console.WriteLine("Массив содержит значение 42: {0}", contains);
+// PrintArray(array);
+
+// void FillArray(int[,,] array)
+// {Random random = new Random();
+//     for (int i = 0; i < 10; i++)
+//     {for (int j = 0; j < 10; j++)
+//         {for (int k = 0; k < 10; k++)
+//             {int value;
+//                 do
+//                 {value = random.Next(10, 1000);}
+//                 while (ArrayContains(array, value));
+//                 array[i, j, k] = value;}}}}
+
+// bool ArrayContains(int[,,] array, int value)
+// {for (int i = 0; i < array.GetLength(0); i++)
+//     {for (int j = 0; j < array.GetLength(1); j++)
+//         {for (int k = 0; k < array.GetLength(2); k++)
+//             {if (array[i, j, k] == value)
+//                 {return true;}}}}
+//         return false;}
+
+// void PrintArray(int[,,] array)
+// {for (int i = 0; i < array.GetLength(0); i++)
+//     {for (int j = 0; j < array.GetLength(1); j++)
+//          {for (int k = 0; k < array.GetLength(2); k++)
+//             {Console.Write("{0}[{1},{2},{3}] ", array[i, j, k], i, j, k);}
+//             Console.WriteLine();}
+//         Console.WriteLine();}}
+
+int[,,] array;
+FillArray(out array);
+PrintArray(array);
+bool contains = ArrayContains(array, 42);
+Console.WriteLine("Массив содержит значение 42: {0}", contains);
+PrintArray(array);
+
+void FillArray(out int[,,] array)
+{
+    array = new int[10, 10, 10];
+    Random random = new Random();
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            for (int k = 0; k < 10; k++)
+            {
+                int value;
+                do
+                {
+                    value = random.Next(10, 1000);
+                } while (ArrayContains(array, value));
+                array[i, j, k] = value;
+            }
+        }
+    }
+}
+
+bool ArrayContains(int[,,] array, int value)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                if (array[i, j, k] == value)
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write("{0}[{1},{2},{3}] ", array[i, j, k], i, j, k);
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Seminar8HomeWork/Zadacha62/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// На входе - натуральное число от пользователя, например 4.
+// Тогда на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+FillSpiralArray(array, 0, 0, m, 1);
+PrintArray(array);
+
+void FillArray(int[,] array)
+ {
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {for (int j = 0; j < array.GetLength(1); j++)
+        {array[i, j] = random.Next(1, 101);}}}
+
+void PrintArray(int[,] array)
+{for (int i = 0; i < array.GetLength(0); i++)
+    {for (int j = 0; j < array.GetLength(1); j++)
+        {Console.Write(array[i, j] + " ");}
+        Console.WriteLine();}}
+Console.WriteLine();
+
+void FillSpiralArray(int[,] array, int x, int y, int size, int num)
+{
+    if (size <= 0) return;
+
+    if (size == 1)
+    {
+        array[x, y] = num;
+        return;
+    }
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        array[x, y + i] = num++;
+    }
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        array[x + i, y + size - 1] = num++;
+    }
+
+    for (int i = size - 1; i > 0; i--)
+    {
+        array[x + size - 1, y + i] = num++;
+    }
+
+    for (int i = size - 1; i > 0; i--)
+    {
+        array[x + i, y] = num++;
+    }
+
+    FillSpiralArray(array, x + 1, y + 1, size - 2, num);
+}
