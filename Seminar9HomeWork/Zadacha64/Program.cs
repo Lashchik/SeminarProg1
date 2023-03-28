f36cc2047e92bdd86442a349579eb2fe9a0d78d7
Aleksandr V. Loshсhilov <Aleksandr.Loshchilov@intourist.ru>
2023-03-28 17:29:40 +0300

    DZ64

---
+++ Seminar9HomeWork/Zadacha64/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Write("Введите N : ");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintNumbers(n);
+
+void PrintNumbers(int n)
+{
+    if (n < 1) return;
+
+    Console.Write(n);
+    if (n > 1)
+    {
+        Console.Write(", ");
+        PrintNumbers(n - 1);
+    }
+}
